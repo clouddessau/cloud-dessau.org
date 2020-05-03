a39e158a44a958db88b9a18b7b3c6922e03eba0e
Johannes Jakob <hi@johjakob.com>
2020-05-03 16:25:37 +0200

    feat: Initialize new Vapid Website

--- js/calendar-link.js
+++
@@ -1,3 +0,0 @@
-if (navigator.platform.match(/(Mac|iPhone|iPod|iPad)/i)) {
-  document.getElementById("a-calendar").setAttribute('href', "webcal://p61-calendars.icloud.com/published/2/Bxi-FeLbmzr52CWv4krZV1wUmwipsRw3U4jzGp_JyhWJE5DpHKAPOBU7VhHe12EMKusK6AewwotNqjPd__PjybDr7qNsqghj47HbCXUWoUo");
-}
